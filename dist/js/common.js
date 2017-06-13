0ef72e457ec9dc72df4976fde8d80f4fc34375c3
yangadmin <18408225637@163.com>
2017-06-13 16:32:02 +0800

    提交代码

--- dist/js/common.js
+++ dist/js/common.js
@@ -2,13 +2,13 @@
  * Created by Administrator on 2017/6/9.
  */
 
-var js=document.scripts;
-var jsPath;
-console.log(js);
-for(var i=js.length;i>0;i--){
-    if(js[i-1].src.indexOf("dist")>-1){
-        console.log(js[i-1].src.lastIndexOf("/"));
-        jsPath=js[i-1].src.substring(0,js[i-1].src.lastIndexOf("/")-7);
-    }
-}
-console.log(jsPath);
+//var js=document.scripts;
+//var jsPath;
+//console.log(js);
+//for(var i=js.length;i>0;i--){
+//    if(js[i-1].src.indexOf("dist")>-1){
+//        console.log(js[i-1].src.lastIndexOf("/"));
+//        jsPath=js[i-1].src.substring(0,js[i-1].src.lastIndexOf("/")-7);
+//    }
+//}
+//console.log(jsPath);
---
+++ dist/keywords.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+
+/**
+ * 搜索引擎的关键字
+ * @param 关键字符的定义
+ * @param data
+ * */
+
+var keyWords = [
+    {
+        name : 'angular',
+        list : ['兼容','初识','学习之路','api','适合']
+    },
+    {
+        name : 'chart',
+        list :['','','','']
+    },
+    {
+        name : 'table',
+        list :['','','','']
+    },
+    {
+        name : 'picture',
+        list :['','','','']
+    },
+    {
+        name : 'chart',
+        list :['','','','']
+    },
+    {
+        name : 'chart',
+        list :['','','','']
+    },
+    {
+        name : 'chart',
+        list :['','','','']
+    },
+    {
+        name : 'chart',
+        list :['','','','']
+    }
+];
